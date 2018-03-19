cbcd45a720bced6852776b5585b799c91e632135
DESKTOP-OUKCBKH\User <saiteki2018@gmail.com>
2018-03-19 23:01:16 +0900

    課題　クラス提出します。

---
+++ CampChallenge_java/CampChallenge_java/013.クラス/kadai_class.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet2;
+
+/**
+ *
+ * @author User
+ */
+public class kadai_class{
+    public static void main(String[]arg){
+             kadai_class nakami=new  kadai_class(); 
+            nakami.main1("sakura","haru");
+           nakami.main2();
+    }
+             
+              public String name="";
+            public String kisetsu;
+            
+           public void main1(String n,String a){
+               
+                this.name=n;
+               this.kisetsu=a;
+                    } 
+             public void main2(){
+                System.out.print(this.name);
+                System.
+                        out.print(this.kisetsu);
+            }
+    
+}
