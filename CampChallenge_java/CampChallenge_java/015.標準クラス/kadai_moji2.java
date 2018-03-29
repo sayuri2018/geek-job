807d7c3190327ce8759d0aab6f9513afd2807d23
DESKTOP-OUKCBKH\User <saiteki2018@gmail.com>
2018-03-29 23:43:53 +0900

    課題　標準クラス　文字列編（文字数の取得）提出します。

---
+++ CampChallenge_java/CampChallenge_java/015.標準クラス/kadai_moji2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet2;
+
+/**
+ *
+ * @author User
+ */
+public class kadai_moji2 {
+             public static void main(String[]args){
+                 
+                 String adress="saiteki2018@gmail.com";
+                 
+                 String adress1=adress.substring(11);
+                 System.out.print(adress1);
+             }
+    
+}
