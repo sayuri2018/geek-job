6934410036583a2ebd67c662ef05aa9c13cde841
DESKTOP-OUKCBKH\User <saiteki2018@gmail.com>
2018-03-29 23:40:34 +0900

    課題　標準クラス　文字列編（バイト数と文字列の取得）提出します。

---
+++ CampChallenge_java/CampChallenge_java/015.標準クラス/kadai_moji1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet2;
+
+/**
+ *
+ * @author User
+ */
+public class kadai_moji1 {
+             public static void main(String[]args){
+                 
+                 String word="奈雲さゆり";
+                 
+                 System.out.println(word.length());
+                 System.out.println(word.getBytes().length);
+             }
+}
